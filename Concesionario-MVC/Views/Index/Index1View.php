430045265a56799aa4975b763f60364b221dd2ec
carlossanchez26 <carlitos.sanchez.cs23@gmail.com>
2024-11-24 16:35:40 +0100

    Delete Concesionario-MVC directory

--- Concesionario-MVC/Views/Index/Index1View.php
+++
@@ -1,3 +0,0 @@
-<div id="contenedor0" class="contenedor0">
-    <h1>MVC - PHP y MySQL</h1>
-</div>
--- Concesionario-MVC/Views/Shared/Footer1View.php
+++
@@ -1,5 +0,0 @@
-<footer class="pie1">
-    <a href="#">Contacto</a>
-    <a href="#">Privacidad</a>
-    <a href="#">MVC - PHP y MySQL</a>
-</footer>
